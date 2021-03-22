a6cbe394ce67fc58c5f6b954d3aa4b87013c76ed
Deepkumar Patel <dspguru97@gmail.com>
2021-03-21 20:09:55 -0400

    local storage

--- js/main.js
+++ js/main.js
@@ -24,6 +24,19 @@ $(document).ready(function(){
         }
         // Else runs if validation is successful
         else{
+
+                //if()
+
+
+            let x = $(".input-email").val();
+            var key = localStorage.getItem(x);
+            if( key == "true"){
+                $(".restricted").css("display", "block");
+                return;
+            }else{
+                $(".restricted").css("display", "none");
+            }
+            localStorage.setItem(x.toString(), "true");
             // console log entered value
             console.log("input email:", email);
             console.log("Drop Down Value:", drop);
